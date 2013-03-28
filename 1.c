18fd5114ebb4dd8dfec44a00633905961cd893e4
Dimko <deemox@gmail.com>
2013-03-28 11:07:45 +0400

    added #1 in c

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  int i, sum = 0;
+
+  for (i = 3; i < 1000; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum = sum + i;
+    }
+  }
+
+  printf("%d\n", sum);
+  return 0;
+}
